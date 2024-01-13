da99949c6e314413825b2fe4c17550e5e6ccb811
MartnReus <martnreus@gmail.com>
2024-01-13 12:49:24 -0300

    shortcut to move between buffers

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -2,11 +2,16 @@ return {
 	-- Monokai Theme
 	{ "tanvirtin/monokai.nvim" },
 
-	-- Configure LazyVim to load gruvbox
+	-- Configure LazyVim to load Monokai
 	{
 		"LazyVim/LazyVim",
 		opts = {
 			colorscheme = "monokai_pro",
 		},
+	},
+
+	-- Surround plugin
+	{
+		"tpope/vim-surround",
 	},
 }
